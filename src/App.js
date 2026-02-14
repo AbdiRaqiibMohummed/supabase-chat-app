7453976d142532f0ecfc503e37d6f0332e0bd473
AbdiRaqiibMohummed <aclipz0121@gmail.com>
2026-02-14 17:18:35 +0000

    added a userSession info like name now creating messaging

--- src/App.js
+++ src/App.js
@@ -1,69 +1,123 @@
 import { useState, useEffect } from "react";
-import { supabase } from "./supabaseClient";
+import { supabase } from "./lib/supabaseClient";
 import { useSession } from "./hooks/useSession";
 
 function App() {
+  const { session } = useSession();
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
+  const [usersOnline, setUsersOnline] = useState([]);
 
-  const { session } = useSession()
+  //useSession hook where i use useEffect
 
-    useEffect(() => {
-      if (!session) {
-        supabase.auth.signInWithOAuth({
-          provider: "google"
-        })
+  // // sign in function
+  const signIn = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: "google",
+    });
+  };
+
+  // channel for supabase to listen to
+  useEffect(() => {
+    if (!session.user) {
+      setUsersOnline([]);
+      return;
+    }
+
+    const roomOne = supabase.channel("room_one", {
+      config: {
+        presence: {
+          key: session?.user?.id,
+        },
+      },
+    });
+
+    //broadcast
+    roomOne.on("broadcast", { event: "message" }, (payload) => {
+      setMessages((prevMessages) => [...prevMessages, payload.payload]);
+      console.log(messages);
+    });
+
+    //track user presence
+    roomOne.subscribe(async (status) => {
+      if (status === "SUBSCRIBED") {
+        await roomOne.track({
+          id: session?.user.id,
+        });
       }
-      // Your effect code here
-    }, [session]);
-    
-  if (!session) {
-    return <p>You need to login </p>
+    });
+
+    // handle user presence
+    roomOne.on("presence", { event: "sync" }, () => {
+      const state = roomOne.presenceState();
+      setUsersOnline(Object.keys(state));
+    });
+
+    // unsubscribe from room
+    return () => {
+      roomOne.unsubscribe();
+    };
+
+  }, [session]);
+
+
+  // send Message
+
+  const sendMessage = async (e) => {
+    e.preventDefault()
   }
 
-// // sign in function
-const signIn = async () => {
+  // // sign out function
+  const signOut = async () => {
+    const { error } = await supabase.auth.signOut();
+  };
 
-}
-
-// // sign out function
-
-const signOut = async () => {
-//   const { error } = supabase.auth.signOut();
-}
-
-
-  return (
-    <div className="w-full flex h-screen justify-center items-center p-4">
-      <div className="border-[1px] border-gray-500 max-w-6xl w-full min-h-[600px] rounded-lg">
-        {/* header */}
-        <div className="flex justify-between h-20 border-b-[1px] border-gray-500">
-          <div className="p-4">
-            <p className="text-gray-300">signed in as name... </p>
-            <p className="text-gray-300 italic text-sm">3 users online</p>
-          </div>
-          <button onClick={signOut} className="bg-gray-800 text-white font-semibold py-2 px-4 rounded m-2 sm:mr-4 ">
-            Sign Out
-          </button>
-        </div>
-        {/* main chat */}
-        <div></div>
-        {/* message input */}
-        <form className="flex  flex-col sm:flex-row p-4 border-t-[1px] border-gray-700">
-          <input
-            type="text"
-            placeholder="Type a message..."
-            className="p-2 w-full bg-[#00000040] rounded-lg"
-          />
-          <button className="mt-4 sm:mt-0 sm:ml-8  bg-gray-500 max-h-12 text-white font-semibold py-2 px-3 rounded">
-            Send
-          </button>
-        </form>
+  if (!session) {
+    return (
+      <div className="w-full flex h-screen justify-center items-center">
+        <button className="p-3 bg-gray-800 rounded-lg" onClick={signIn}>
+          Sign in with Google to chat
+        </button>
       </div>
-    </div>
-  );
-
-
-
- 
+    );
+  } else {
+    return (
+      <div className="w-full flex h-screen justify-center items-center p-4">
+        <div className="border-[1px] border-gray-500 max-w-6xl w-full min-h-[600px] rounded-lg">
+          {/* header */}
+          <div className="flex justify-between h-20 border-b-[1px] border-gray-500">
+            <div className="p-4">
+              <p className="text-gray-300">
+                Signed in as {session?.user?.user_metadata?.full_name}{" "}
+              </p>
+              <p className="text-gray-300 italic text-sm">3 users online</p>
+            </div>
+            <button
+              onClick={signOut}
+              className="bg-gray-800 text-white font-semibold py-2 px-4 rounded m-2 sm:mr-4 "
+            >
+              Sign Out
+            </button>
+          </div>
+          {/* main chat */}
+          <div className="p-4 flex flex-col overflow-y-auto h-[500px]"></div>
+          {/* message input */}
+          <form className="flex  flex-col sm:flex-row p-4 border-t-[1px] border-gray-500">
+            <input
+              value={newMessage}
+              onChange={(e) => setNewMessage(e.target.value)}
+              type="text"
+              placeholder="Type a message..."
+              className="p-2 w-full bg-[#00000040] rounded-lg"
+            />
+            <button className="mt-4 sm:mt-0 sm:ml-8  bg-gray-500 max-h-12 text-white font-semibold py-2 px-3 rounded">
+              Send
+            </button>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/hooks/useSession.js
+++ src/hooks/useSession.js
@@ -1,27 +1,30 @@
-
-import { supabase } from "../supabaseClient";
-import { useEffect,useState } from "react";
+import { supabase } from "../lib/supabaseClient";
+import { useEffect, useState } from "react";
 
 export const useSession = () => {
-      // eslint-disable-next-line no-undef
-      const [session, setSession] = useState([]);
-     
-      useEffect(() => {        
-        // Get initial session
-        supabase.auth.getSession().then(({ data: { session } }) => {
-          setSession(session);
-        });
-    
-        const {
-          data: { subscription },
-        } = supabase.auth.onAuthStateChange((_event, session) => {
-          setSession(session);
-        });
-    
-        return () => subscription.unsubscribe();
-      }, []);
+  // eslint-disable-next-line no-undef
+  const [session, setSession] = useState([]);
+  
+ 
+  useEffect(() => {
+    // Get initial session
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
 
-      return {
-        session
-      }
-}
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  console.log(session);
+
+  return {
+    session,
+    
+  };
+};
