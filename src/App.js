ec14135b28164fcfdd1bab8659c310039213cbfc
AbdiRaqiibMohummed <aclipz0121@gmail.com>
2026-02-16 17:17:30 +0000

    Functional chat app need to store data next

--- src/App.js
+++ src/App.js
@@ -1,43 +1,51 @@
 import { useState, useEffect } from "react";
 import { supabase } from "./lib/supabaseClient";
 import { useSession } from "./hooks/useSession";
+import { useRef } from "react";
+
+
+
 
 function App() {
+  const channelRef = useRef(null);
   const { session } = useSession();
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState("");
   const [usersOnline, setUsersOnline] = useState([]);
-  
-  
+
   useEffect(() => {
     if (!session.user) {
-        setUsersOnline([]);
-        return;
+      setUsersOnline([]);
+      return;
     }
 
     const roomOne = supabase.channel("room_one", {
-        config: {
-            presence: {
-                key: session?.user?.id,
-            },
+      config: {
+        presence: {
+          key: session?.user?.id,
         },
+      },
     });
+
+    channelRef.current = roomOne;
 
     //broadcast
     roomOne.on("broadcast", { event: "message" }, (payload) => {
-        setMessages((prevMessages) => [...prevMessages, payload.payload]);
-        console.log(messages);
+      setMessages((prevMessages) => [...prevMessages, payload.payload]);
     });
 
     //track user presence
     roomOne.subscribe(async (status) => {
-        if (status === "SUBSCRIBED") {
-            await roomOne.track({
-                id: session?.user.id,
-            });
-        }
+      if (status === "SUBSCRIBED") {
+        await roomOne.track({
+          id: session?.user.id,
+        });
+      }
     });
-}, [messages,session?.user]); 
+    return () => {
+      roomOne.unsubscribe();
+    }
+  }, [session?.user]);
 
   //useSession hook where i use useEffect
 
@@ -54,7 +62,31 @@ function App() {
 
   const sendMessage = async (e) => {
     e.preventDefault();
-    console.log("sent message")
+
+    if(!channelRef.current) return;
+
+    const messagePayload = {
+      message: newMessage,
+      user_name: session?.user?.user_metadata?.email,
+      avatar: session?.user?.user_metadata?.avatar,
+      timestamp: new Date().toISOString(),
+    };
+
+    setMessages((prevMessages) => [...prevMessages , messagePayload]);
+
+    await channelRef.current.send({
+      type: "broadcast",
+      event: "message",
+      payload: {
+        message: newMessage,
+        user_name: session?.user?.user_metadata?.email,
+        avatar: session?.user?.user_metadata?.avatar,
+        timestamp: new Date().toISOString(),
+      },
+    });
+
+    setNewMessage("")
+    
   };
 
   // // sign out function
@@ -90,9 +122,18 @@ function App() {
             </button>
           </div>
           {/* main chat */}
-          <div className="p-4 flex flex-col overflow-y-auto h-[500px]"></div>
+          <div className="p-4 flex flex-col overflow-y-auto h-[500px]">
+            {messages.map((msg, idx) => (
+              <div>
+                
+              </div>
+            ))}
+          </div>
           {/* message input */}
-          <form className="flex  flex-col sm:flex-row p-4 border-t-[1px] border-gray-500">
+          <form
+            onSubmit={sendMessage}
+            className="flex  flex-col sm:flex-row p-4 border-t-[1px] border-gray-500"
+          >
             <input
               value={newMessage}
               onChange={(e) => setNewMessage(e.target.value)}
@@ -100,7 +141,11 @@ function App() {
               placeholder="Type a message..."
               className="p-2 w-full bg-[#00000040] rounded-lg"
             />
-            <button onClick={sendMessage} className="mt-4 sm:mt-0 sm:ml-8  bg-gray-500 max-h-12 text-white font-semibold py-2 px-3 rounded">
+            <button
+             onClick={sendMessage}
+              type="submit"
+              className="mt-4 sm:mt-0 sm:ml-8  bg-gray-500 max-h-12 text-white font-semibold py-2 px-3 rounded"
+            >
               Send
             </button>
           </form>
